728b679c9e736fe4565762c2dee752ac91bd94e2
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-02 12:40:49 -0500

    Server working on local host with dummy data sent and received from database

--- db/index.js
+++ db/index.js
@@ -4,7 +4,40 @@ const mysqlConfig = require('./config.js');
 var connection = mysql.createConnection(mysqlConfig);
 
 showFavoritedMeals = function (callback) {
-    connection.query(`SELECT * from MealRecipes`, function (error, results) {
+    connection.query(`SELECT * from MealRecipes`, (error, results) => {
+        if (error) {
+            callback(error, null);
+        } else {
+            for (let i = 0; i < results.length; i++) {
+                results[i].name = results[i].name.split('---')[1];
+            }
+            callback(null, results);
+        }
+    })
+}
+
+addFavoritedMeal = function (meal, callback) {
+    connection.query(`INSERT IGNORE INTO MealRecipes (name, instructions, image, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15, ingredient16, ingredient17, ingredient18, ingredient19, ingredient20, measurement1, measurement2, measurement3, measurement4, measurement5, measurement6, measurement7, measurement8, measurement9, measurement10, measurement11, measurement12, measurement13, measurement14, measurement15, measurement16, measurement17, measurement18, measurement19, measurement20, user) Values ("mfuechec---Teriyaki Chicken Casserole", "Preheat oven to 350° F. Spray a 9x13-inch baking pan with non-stick spray. Combine soy sauce, ½ cup water, brown sugar, ginger and garlic in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling. Meanwhile, stir together the corn starch and 2 tablespoons of water in a separate dish until smooth. Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat. Place the chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove from oven and shred chicken in the dish using two forks. *Meanwhile, steam or cook the vegetables according to package directions. Add the cooked vegetables and rice to the casserole dish with the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!", "https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg", "soy sauce", "water", "brown sugar", "ground ginger", "minced garlic", "cornstarch", "chicken breasts", "stir-fry vegetables", "brown rice", "", "", "", "", "", "", "", "", "", "", "", "3/4 cup", "1/2 cup", "1/4 cup", "1/2 teaspoon", "1/2 teaspoon", "4 Tablespoons", "2", "1 (12 oz.)", "3 cups", "", "", "", "", "", "", "", "", "", "", "", "mfuechec")`, function (error, results) {
+        if (error) {
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    })
+}
+
+deleteFavoritedMeal = function (meal, callback) {
+    connection.query(`DELETE from MealRecipes where name = "mfuechec---Teriyaki Chicken Casserole"`, function (error, results) {
+        if (error) {
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    })
+}
+
+updateFavoritedMeal = function (meal, callback) {
+    connection.query(`UPDATE MealRecipes SET name="mfuechec---Chicken Teriyaki Bake" where id = "6"`, function (error, results) {
         if (error) {
             callback(error, null);
         } else {
@@ -14,7 +47,20 @@ showFavoritedMeals = function (callback) {
 }
 
 showFavoritedDrinks = function (callback) {
-    connection.query(`SELECT * from DrinkRecipes`, function (error, results) {
+    connection.query(`SELECT * from DrinkRecipes`, (error, results) => {
+        if (error) {
+            callback(error, null);
+        } else {
+            for (let i = 0; i < results.length; i++) {
+                results[i].name = results[i].name.split('---')[1];
+            }
+            callback(null, results);
+        }
+    })
+}
+
+addFavoritedDrink = function (drink, callback) {
+    connection.query(`INSERT IGNORE INTO DrinkRecipes (name, instructions, image, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15, ingredient16, ingredient17, ingredient18, ingredient19, ingredient20, measurement1, measurement2, measurement3, measurement4, measurement5, measurement6, measurement7, measurement8, measurement9, measurement10, measurement11, measurement12, measurement13, measurement14, measurement15, measurement16, measurement17, measurement18, measurement19, measurement20, user) Values ("mfuechec---Teriyaki Chicken Casserole", "Preheat oven to 350° F. Spray a 9x13-inch baking pan with non-stick spray. Combine soy sauce, ½ cup water, brown sugar, ginger and garlic in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling. Meanwhile, stir together the corn starch and 2 tablespoons of water in a separate dish until smooth. Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat. Place the chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove from oven and shred chicken in the dish using two forks. *Meanwhile, steam or cook the vegetables according to package directions. Add the cooked vegetables and rice to the casserole dish with the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!", "https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg", "soy sauce", "water", "brown sugar", "ground ginger", "minced garlic", "cornstarch", "chicken breasts", "stir-fry vegetables", "brown rice", "", "", "", "", "", "", "", "", "", "", "", "3/4 cup", "1/2 cup", "1/4 cup", "1/2 teaspoon", "1/2 teaspoon", "4 Tablespoons", "2", "1 (12 oz.)", "3 cups", "", "", "", "", "", "", "", "", "", "", "", "mfuechec")`, function (error, results) {
         if (error) {
             callback(error, null);
         } else {
@@ -23,62 +69,24 @@ showFavoritedDrinks = function (callback) {
     })
 }
 
-addFavoritedMeal = function (meal, callback) {
-    // This function must first check that the entry doesn't already exist, then, if not, add it
-    // Returns error, entry already exists or successfully added to favorites
-
-    connection.query(`COUNT * from MealRecipes where id = meal.id`, function (error, results) {
-        if (error) {
-            callback(error, null);
-        } else {
-            if (results !== 0) {
-                let error = new Error('Entry already exists')
-            } else {
-                // Insert into the database
-            }
-        }
-    })
-}
-
-addFavoritedDrink = function (drink, callback) {
-    // This function must first check that the entry doesn't already exist, then, if not, add it
-    // Returns error, entry already exists or successfully added to favorites
-
-    connection.query(`COUNT * from DrinkRecipes where id = drink.id`, function (error, results) {
-        if (error) {
-            callback(error, null);
-        } else {
-            if (results !== 0) {
-                let error = new Error('Entry already exists')
-            } else {
-                // Insert into the database
-            }
-        }
-    })
-}
-
-deleteFavoritedMeal = function (meal, callback) {
-    // This function deletes a meal from the favorited list
-    // Returns error, no entry found or successfully deleted from favorites
-    callback(null, 'Called deleteFavoritedMeal');
-}
-
 deleteFavoritedDrink = function (drink, callback) {
-    // This function deletes a drink from the favorited list
-    // Returns error, no entry found or successfully deleted from favorites
-    callback(null, 'Called deleteFavoritedDrink');
-}
-
-updateFavoritedMeal = function (meal, callback) {
-    // This function finds a meal by given mealId, then changes all its data based on body
-    // Return errors, no entry found, or successfully updated recipe
-    callback(null, 'Called updateFavoritedMeals');
+    connection.query(`DELETE from DrinkRecipes where name = "mfuechec---Teriyaki Chicken Casserole"`, function (error, results) {
+        if (error) {
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    })
 }
 
 updateFavoritedDrink = function (drink, callback) {
-    // This function finds a drink by given drinkId, then changes all its data based on body
-    // Return errors, no entry found, or successfully updated recipe
-    callback(null, 'Called updateFavoritedDrinks');
+    connection.query(`UPDATE DrinkRecipes SET name="mfuechec---Chicken Teriyaki Bake" where id = "2"`, function (error, results) {
+        if (error) {
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    })
 }
 
 module.exports.showFavoritedMeals = showFavoritedMeals;
