f50ea36376467837a1974d7e3ca183a2b140c9ac
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-07 12:58:30 -0500

    Changed headers

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     };
     next();
